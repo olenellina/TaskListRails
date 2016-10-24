2ca8e5ccd6df3e709fdabc2daae80368402aca04
Melissa Rodriguez <olenellina@gmail.com>
2016-10-24 13:44:29 -0700

    adding validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username,
+            presence: true,
+            uniqueness: true
+
+  validates :email,
+            presence: true,
+            uniqueness: true
 
   def self.build_from_github(auth_hash)
     user       = User.new
